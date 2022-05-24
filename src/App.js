d08364fb359d620baad41248f2200c6ffbb54fee
nishattasnim08 <nishattasnim24258@gmail.com>
2022-05-24 23:13:25 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Pages/Shared/Header/Header';
 import { Routes, Route, Link } from "react-router-dom";
 import Tools from './Pages/Home/Tools/Tools';
 import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="home" element={<Home />} />
         <Route path="tools" element={<Tools />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
