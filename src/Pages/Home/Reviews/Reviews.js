3f8b81b9c6d0d39dca893679f7eca3acccce93f7
nishattasnim08 <nishattasnim24258@gmail.com>
2022-05-28 20:42:59 +0600

    rating displayed on home page

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Devider from '../../Shared/Devider/Devider';
 import './Reviews.css';
 
 const Reviews = () => {
+
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/review")
+            .then((res) => res.json())
+            .then((data) => setReviews(data));
+    }, []);
+
     return (
         <div class="my-10" id='reviews'>
             <div class="mx-20 my-10 text-center">
                 <h1 class="py-6 text-5xl md:text-7xl">Customer Reviews</h1>
                 <Devider />
             </div>
-            <div class="my-10 p-5 review-section">
-                <div class="flex items-center">
-                    <div class="avatar">
-                        <div class="w-24 rounded-full">
-                            <img src="https://img.freepik.com/free-photo/portrait-young-businesswoman-holding-eyeglasses-hand-against-gray-backdrop_23-2148029483.jpg?t=st=1653672700~exp=1653673300~hmac=6a538b372cd6694f1bc2e5a08acaa2a62653833e75a005fd8d7c178eca02061f&w=740" />
+            {
+                reviews.map(review => <div class="my-10 p-5 review-section">
+                    <div class="flex items-center">
+                        <div class="avatar">
+                            <div class="w-24 rounded-full">
+                                <img src={review.image} />
+                            </div>
+                        </div>
+                        <div class=" mx-10 rating gap-1">
+                            {
+                                Array.from(Array(review.rating).keys()).map(i => <input type="radio" name="rating-3" class="mask mask-heart bg-orange-400" disabled checked />)
+                            }
+                            {
+                                Array.from(Array(5-review.rating).keys()).map(i => <input type="radio" name="rating-3" class="mask mask-heart bg-stone-500" disabled/>)
+                                
+                            }
                         </div>
                     </div>
-                    <div class=" mx-10 rating gap-1">
-                        {
-                            Array.from(Array(3).keys()).map(i => <input type="radio" name="rating-3" class="mask mask-heart bg-orange-400" checked />)
-                        }
-                        {
-                            Array.from(Array(3).keys()).map(i => <input type="radio" name="rating-3" class="mask mask-heart bg-red-400" />)
-                        }
-                    </div>
-                </div>
-                <p class="p-5 text-2xl md:text-4xl leading-relaxed">I love to use their products. They supplies the most qualityful products</p>
-            </div>
-            
+                    <p class="p-5 text-2xl md:text-4xl leading-relaxed">{review.reviewText}</p>
+                </div>)
+
+            }
+
         </div>
     );
 };
